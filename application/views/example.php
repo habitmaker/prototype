0ae7adaf8292a991392cdae69cbc215f060bd81d
Sam Duke <mail@samjohnduke.com>
2012-05-05 10:06:12 +1000

    added more stuff

---
+++ application/views/example.php
@@ -0,0 +1,41 @@
+<?php 
+	/**
+	 * Write a simple one or two sentences of what you plan to achieve
+	 * with this filea. 
+	 * 
+	 * Author: //fill this in with the people who are working on this file
+	 * Date: //WHat is todays date
+	 * 
+	 * Version $Revision$
+	 */
+?>
+
+<!DOCTYPE html>
+
+<!-- Head contains real basic stuff - don't put any display stuff here -->
+<head>
+	
+	<meta charset="utf-8">
+	<title><?php $page_title; ?></title>
+	
+	<link href='../../resource/css/style.css' />
+	
+</head>
+
+<body>
+	
+	
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/libs/jquery-1.7.1.min.js"><\/script>')</script>
+    <script src='../../resource/js/main.js'></script>
+    
+    <!-- Asynchronous Google Analytics snippet. Change UA-XXXXX-X to be your site's ID.
+       mathiasbynens.be/notes/async-analytics-snippet -->
+    <script>
+      var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+      (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+      g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+      s.parentNode.insertBefore(g,s)}(document,'script'));
+    </script>
+    
+</body>
--- application/views/home.php
+++ application/views/home.php
@@ -3,9 +3,15 @@
 <head>
 	<meta charset="utf-8">
 	<title>Habit Maker | Healthy Habit Former</title>
+	<link rel='stylesheet' href='../../resource/css/style.css' />
+	<script src="../../resource/js/modernizr-2.5.3.min.js"></script>
 </head>
 <body>
-	<h1>Habit Maker</h1>
+	<h1>Habits</h1>
 	<h2>Mind Body Soul</h2>
+	
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/libs/jquery-1.7.1.min.js"><\/script>')</script>
+    <script src='../../resource/js/main.js'></script>
 </body>
 </html>
